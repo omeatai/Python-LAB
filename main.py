091992dfd7abab583e3ee373e65dd0fa23a7cac9
IFEANYI OMEATA <omeatai@gmail.com>
2022-06-19 13:11:19 +0000

    pop

--- main.py
+++ main.py
@@ -84,9 +84,12 @@ numbers.append(7)
 print(numbers)
 numbers.insert(2,100)
 print(numbers)
-numbers.extend([10,11,12,13,14])
+numbers.extend([1,2,3,10,11,12,13,14])
 print(numbers)
-
+numbers.pop()
+print(numbers)
+numbers.pop(3)
+print(numbers)
 
 
 
